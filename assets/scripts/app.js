38ca53b2c0854e74e18e30b6b050298495f22367
Dave Santore <david.santore@gmail.com>
2019-06-14 14:12:14 -0400

    Revert "Handlebars"

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,9 +5,7 @@
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
-const events = ('./events.js')
 
 $(() => {
   // your JS code goes here
-  $('.remove-book').on('click', deleteBooks)
 })
